41b3e5dfd4e7d3066ac51151a8eb51952c485b9e
gameboyprinter <gameboyprinter@users.noreply.github.com>
2018-08-03 22:06:04 -0500

    adjustable contrast

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ var xData = [],
 
 var input;
 var img;
+var saturation;
+var contrast;
+var url;
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     input = document.getElementById("file");
     input.addEventListener('change', upload);
+    contrast = document.getElementById("contrast").value;
+    document.getElementById("contrast").addEventListener("change", contrastChange);
+    saturation = document.getElementById("saturation").value;
+    document.getElementById("saturation").addEventListener("change", saturationChange);
+}
+
+async function saturationChange(e) {
+    saturation = e.target.value;
+    await draw(img);
+    analyze(img);
+}
+
+async function contrastChange(e) {
+    contrast = e.target.value;
+    await draw(img);
+    analyze(img);
 }
 
 function erase() {
@@ -26,8 +45,6 @@ function erase() {
 }
 
 function analyze() {
-    if(typeof img === "undefined")
-        return;
     Tesseract.recognize(canvas)
         .progress(message => {
             var status = document.getElementById("status");
@@ -40,24 +57,41 @@ function analyze() {
         });
 }
 
-function upload(e) {
-    img = new Image();
-    img.src = window.URL.createObjectURL(e.target.files[0]);
-    erase();
-    img.onload = () => {
-        img.width *= 5;
-        img.height *= 5;
-        if(img.width > 1920){
-            img.height *= Math.floor(img.width/1920);
-            img.width = 1920;
+function draw(img) {
+    if (url == "")
+        return;
+    return new Promise(resolve => {
+        erase();
+        img = new Image();
+        img.src = url;
+        img.onload = () => {
+            img.width *= 5;
+            img.height *= 5;
+            if (img.width > 1920) {
+                img.height *= Math.floor(img.width / 1920);
+                img.width = 1920;
+            }
+            if (img.height > 1080) {
+                img.height = 1080;
+            }
+
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+
+
+            ctx.filter = `saturate(${saturation}%) contrast(${contrast}%)`;
+            console.log(ctx.filter);
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            resolve();
         }
-        if(img.height > 1080){
-            img.height = 1080;
-        }
-            
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-        analyze();
-    };
+    });
 }
+
+async function upload(e) {
+    url = window.URL.createObjectURL(e.target.files[0]);
+    await draw(img);
+    analyze(img);
+}
+
+//210-788-2727
