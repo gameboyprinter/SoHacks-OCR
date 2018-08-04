a9c170b60ade569cdcd97d2ea2b8a4a3f3fe7669
gameboyprinter <gameboyprinter@users.noreply.github.com>
2018-08-04 03:09:35 -0500

    cropping

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var canvas, ctx;
 
 var isDown = false;
-var prevX = 0,
-    currX = 0,
-    prevY = 0,
-    currY = 0;
-var xData = [],
-    yData = [];
+var startX = 0;
+var startY = 0;
 
 var input;
 var img;
@@ -17,12 +13,87 @@ var url;
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    canvas2 = document.getElementById('canvas2');
+    ctx2 = canvas2.getContext('2d');
     input = document.getElementById("file");
     input.addEventListener('change', upload);
     contrast = document.getElementById("contrast").value;
     document.getElementById("contrast").addEventListener("change", contrastChange);
     saturation = document.getElementById("saturation").value;
     document.getElementById("saturation").addEventListener("change", saturationChange);
+
+    canvas2.addEventListener('mousemove', mousemove, false);
+    canvas2.addEventListener('mousedown', mousedown, false);
+    canvas2.addEventListener('mouseup', mouseup, false);
+    canvas2.addEventListener('mouseout', mouseup, false);
+
+    canvas2.x = canvas.x;
+    canvas2.y = canvas.y;
+    document.getElementById("canvasses").style = `height: ${canvas.height};`;
+}
+
+function getXY() {
+    var x = 0;
+    var y = 0;
+    var o = canvas2;
+    while (o) {
+        x += o.offsetLeft;
+        y += o.offsetTop;
+        o = o.offsetParent;
+    }
+    try {
+        x -= document.scrollingElement.scrollLeft;
+        y -= document.scrollingElement.scrollTop;
+    } catch (e) { /* MSIE? */ }
+    return {
+        x: x,
+        y: y
+    };
+}
+
+
+function mouseup(e) {
+
+    ctx2.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (isDown) {
+        img = new Image();
+        img.src = canvas.toDataURL("image/png");
+        console.log(img.src);
+        img.onload = () => {
+            ctx.filter = `saturate(${saturation}%) contrast(${contrast}%)`;
+
+            var xy = getXY();
+            canvas.height = (e.clientY - xy.y) - startY;
+            canvas.width = (e.clientX - xy.x) - startX;
+            canvas2.height = canvas.height;
+            canvas2.width = canvas.width;
+            document.getElementById("canvasses").style = `height: ${canvas.height};`;
+
+            erase();
+            ctx.drawImage(img, -startX, -startY);
+
+            analyze();
+        }
+    }
+
+    isDown = false;
+}
+
+function mousedown(e) {
+    isDown = true;
+    var xy = getXY();
+    startX = e.clientX - xy.x;
+    startY = e.clientY - xy.y;
+}
+
+function mousemove(e) {
+    ctx2.clearRect(0, 0, canvas.width, canvas.height);
+    if (isDown) {
+        ctx2.strokeStyle = "black";
+        var xy = getXY();
+        ctx2.strokeRect(startX, startY, (e.clientX - xy.x) - startX, (e.clientY - xy.y) - startY);
+    }
 }
 
 async function saturationChange(e) {
@@ -40,8 +111,6 @@ async function contrastChange(e) {
 function erase() {
     // Erase.
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    xData.length = 0;
-    yData.length = 0;
 }
 
 function analyze() {
@@ -77,11 +146,11 @@ function draw(img) {
 
             canvas.width = img.width;
             canvas.height = img.height;
-
-
+            canvas2.width = canvas.width;
+            canvas2.height = canvas.height;
+            document.getElementById("canvasses").style = `height: ${canvas.height};`;
 
             ctx.filter = `saturate(${saturation}%) contrast(${contrast}%)`;
-            console.log(ctx.filter);
             ctx.drawImage(img, 0, 0, img.width, img.height);
             resolve();
         }
